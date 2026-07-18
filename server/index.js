22561a367b1ecb42e7ea1208fbe59454cc583f43
Rajat Savdekar <156324870+RDX-Rajat-Savdekar@users.noreply.github.com>
2026-07-18 15:36:32 -0700

    fix: allow x-passkey in CORS preflight headers

--- server/index.js
+++ server/index.js
@@ -121,7 +121,7 @@ setPersistence({
 const server = http.createServer((req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, x-passkey')
 
   if (req.method === 'OPTIONS') { res.writeHead(204); res.end(); return }
 
